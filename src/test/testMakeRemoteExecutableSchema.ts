f81b20ebff3c5249306eeb593c7c3f9367ec92be
Oliver Wright <meiamsome@meiamso.me>
2020-04-06 01:12:21 +0100

    Failing tests

--- src/test/testMakeRemoteExecutableSchema.ts
+++ src/test/testMakeRemoteExecutableSchema.ts
@@ -2,7 +2,7 @@
 
 import { expect } from 'chai';
 import { forAwaitEach } from 'iterall';
-import { GraphQLSchema, ExecutionResult, subscribe, parse } from 'graphql';
+import { GraphQLSchema, ExecutionResult, subscribe, parse, execute, print } from 'graphql';
 import {
   subscriptionSchema,
   subscriptionPubSubTrigger,
@@ -10,6 +10,7 @@ import {
   makeSchemaRemoteFromLink
 } from '../test/testingSchemas';
 import { makeRemoteExecutableSchema } from '../stitching';
+import { FetcherOperation } from '../stitching/makeRemoteExecutableSchema';
 
 describe('remote subscriptions', () => {
   let schema: GraphQLSchema;
@@ -116,5 +117,84 @@ describe('respects buildSchema options', () => {
     expect(field.description).to.eq('Field description');
     const customScalar = remoteSchema.getType('CustomScalar');
     expect(customScalar.description).to.eq('Scalar description');
+  });
+});
+
+describe('when query for multiple fields', () => {
+  const schema = `
+    type Query {
+      fieldA: Int!
+      fieldB: Int!
+      field3: Int!
+    }
+  `;
+  const query = parse(`
+    query {
+      fieldA
+      fieldB
+      field3
+    }
+  `);
+  let calls: FetcherOperation[] = [];
+  const fetcher = (args: FetcherOperation) => {
+    calls.push(args);
+    return Promise.resolve({
+      data: {
+        fieldA: 1,
+        fieldB: 2,
+        field3: 3,
+      }
+    });
+  };
+  const remoteSchema = makeRemoteExecutableSchema({
+    fetcher,
+    schema,
+  });
+
+  beforeEach(() => {
+    calls = [];
+  });
+
+  // One of the two tests below should work depending upon what the correct intended behaviour is
+  it('forwards one upstream query', async () => {
+    const result = await execute(remoteSchema, query);
+    expect(result).to.deep.equal({
+      data: {
+        fieldA: 1,
+        fieldB: 2,
+        field3: 3,
+      }
+    })
+
+    expect(calls).to.have.length(1);
+    expect(print(calls[0].query)).to.be.equal(print(query));
+  });
+
+  it('forwards three upstream queries', async () => {
+    const result = await execute(remoteSchema, query);
+    expect(result).to.deep.equal({
+      data: {
+        fieldA: 1,
+        fieldB: 2,
+        field3: 3,
+      }
+    })
+
+    expect(calls).to.have.length(3);
+    expect(print(calls[0].query)).to.be.equal(`\
+{
+  fieldA
+}
+`);
+expect(print(calls[1].query)).to.be.equal(`\
+{
+  fieldB
+}
+`);
+expect(print(calls[2].query)).to.be.equal(`\
+{
+  field3
+}
+`);
   });
 });
